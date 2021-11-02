f53010171e9acb2864a19d0b31563776253a3604
a21solge <sol20glez@gmail.com>
2021-11-02 08:55:54 +0100

    Primeiro commit de olamundo - Sol González

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "odoo_olamundo",
+
+    'summary': """
+        Proxecto inicial ola_mundo por Sol González""",
+
+    'description': """
+        Proxecto para iniciarnos en odoo
+    """,
+
+    'author': "a21solge",
+    'website': "http://www.edu.xunta.gal/centros/iesteis",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
